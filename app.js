9038eabcc669ca2e0cb8d4dc397484efe9aa2cd0
Heinti <olegukraine33@gmail.com>
2023-05-26 20:32:03 +0300

    05 works done

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public")) //брати в цій папці, коли прийшов запит на статичний файл
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,12 @@
 const validateBody = require('./validateBody')
 const isValidid = require('./isValidid')
 const authenticate = require("./authenticate")
+const upload = require("./upload")
 
 module.exports = {
     validateBody,
     isValidid,
     authenticate,
+    upload,
+    
 }
